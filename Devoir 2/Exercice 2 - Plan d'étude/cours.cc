7731a1d55ab56cbdf31523aed624f05c0e05df44
Julien Matthey <jumatthey@bluewin.ch>
2015-03-13 19:12:59 +0100

    Plan d'études en cours

--- Devoir 2/Exercice 2 - Plan d'étude/cours.cc
+++ Devoir 2/Exercice 2 - Plan d'étude/cours.cc
@@ -61,11 +61,198 @@ typedef string CourseId;
  * Complétez le programme à partir d'ici.
  *******************************************/
 
+class Activity {
 
-    cout << ", durée ";
+private:
+
+    string lieu;
+    double duree;
+    Time horraire;
+
+public:
+
+    Activity (const string& lieu, const Day& jour, const double& heure, const double& duree) : lieu{lieu}, horraire{jour, heure}, duree{duree} {}
+    Activity () : horraire("", 0.) {};
+    Activity (const Activity& a) = delete;
+    Activity operator = (Activity& a) = delete;
+
+    string getLocation () const {
+        return this -> lieu;
+    }
+
+    Time getTime () const {
+        return this -> horraire;
+    }
+
+    double getDuration () const {
+        return this -> duree;
+    }
+
+    bool conflicts (const Activity& a) const {
+        if (
+            a.horraire.day() == this -> horraire.day() &&
+            (
+                a.horraire.hour() + a.getDuration() > this -> horraire.hour() ||
+                this -> horraire.hour() + this -> getDuration() > a.horraire.hour()
+            )
+        ) return true;
+        else return false;
+    }
+
+    void print () const {
+        cout << "le ";
+        this -> horraire.print();
+        cout << " en " << this -> lieu << ", durée ";
+        print_time(this -> duree);
+    }
+};
+
+class Course {
+
+private:
+
+    CourseId id;
+    string nom;
+    Activity cours;
+    Activity ex;
+    int credits;
+
+public:
+
+    Course (const CourseId& id, const string& nom, const Activity& cours, const Activity& ex, const int& credits) :
+            id{id},
+            nom{nom},
+            cours{cours.getLocation(), cours.getTime().day(), cours.getTime().hour(), cours.getDuration()},
+            ex{ex.getLocation(), ex.getTime().day(), ex.getTime().hour(), ex.getDuration()},
+            credits{credits}
+    {
+        cout << "Nouveau cours : " << this -> id << endl;
+    }
+
+    Course (const Course& c) = delete;
+    Course operator = (Course& c) = delete;
+
+    ~Course () {
+        cout << "Suppression du cours : " << this -> id << endl;
+    }
+
+    CourseId getId () const {
+        return this -> id;
+    }
+
+    string getTitle () const {
+        return this -> nom;
+    }
+
+    int getCredits () const {
+        return this -> credits;
+    }
+
+    double workload () const {
+        return this -> cours.getDuration() + ex.getDuration();
+    }
+
+    bool conflicts (const Activity& a) const {
+        if (cours.conflicts(a) || ex.conflicts(a)) return true;
+        else return false;
+    }
+
+    bool conflicts (const Course& c) const {
+        if (this -> conflicts(c.cours) || this -> conflicts(c.ex)) return true;
+        else return false;
+    }
+
+    void print () const {
+        cout << this -> id << ": " << this -> nom << " - cours ";
+        this -> cours.print();
+        cout << ", exercices ";
+        this -> ex.print();
+        cout << ". crédits : " << this -> credits;
+    }
+};
+
+class StudyPlan {
+
+private:
+
+    vector<const Course*> vcp;
+
+    const Course& findCourse (const CourseId& cId) const {
+        for (const Course* cp : this -> vcp) {
+            if (cp -> getId() == cId) return *cp;
+        }
+    }
+
+public:
+
+    void add_course (const Course& c) {
+        this -> vcp.push_back (&c);
+    }
+
+    bool conflicts (const CourseId id, vector<CourseId> vid) const {
+        for (const Course* cp : this -> vcp) {
+            if (cp -> getId() == id) {
+                for (const Course* cp2 : this -> vcp) {
+                    for (CourseId cId : vid) {
+                        if (
+                            cp2 -> getId() == cId &&
+                            cp2 -> getId() != id &&
+                            cp -> conflicts(*cp2)
+                        ) return true;
+                    }
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+    void print (const CourseId& cId) const {
+        for (const Course* cp : this -> vcp) {
+            if (cp -> getId() == cId) cp -> print();
+        }
+    }
+
+    int credits (const CourseId& cId) const {
+        for (const Course* cp : this -> vcp) {
+            if (cp -> getId() == cId) return cp -> getCredits();
+        }
+        return 0;
+    }
+
+    double workload (const CourseId& cId) const {
+        for (const Course* cp : this -> vcp) {
+            if (cp -> getId() == cId) return cp -> workload();
+        }
+        return 0.d;
+    }
+
+    void printCourseSuggestions (vector<CourseId> vid) const {
+        int compatible = 0;
+        bool conflit = false;
+        for (const Course* cp : this -> vcp) {
+            for (const CourseId cId : vid) {
+                if (this -> conflicts(cId, vid)) conflit = true;
+            }
+            if (!conflit) {
+                cp -> print();
+                cout << endl;
+                ++compatible;
+                break;
+            }
+            else conflit = false;
+        }
+        if (!compatible) cout << "Aucun cours n'est compatible avec la sélection de cours." << endl;
+    }
+};
+
+class Schedule {
+    // Hello World
+};
 
 
-    cout << ". crédits : ";
+
+
 
 
       cout << "Aucun cours n'est compatible avec la sélection de cours." << endl;
